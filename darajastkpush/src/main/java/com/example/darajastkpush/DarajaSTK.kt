329afe8f86cd58664e1a3d82a11db30f8bf3bb68
Uncharted <mainae835@gmail.com>
2025-02-01 05:44:44 +0300

    a refactor

--- darajastkpush/src/main/java/com/example/darajastkpush/DarajaSTK.kt
+++ darajastkpush/src/main/java/com/example/darajastkpush/DarajaSTK.kt
@@ -4,9 +4,8 @@ import android.content.Context
 import android.util.Base64
 import com.example.darajastkpush.models.STKPushRequest
 import com.example.darajastkpush.models.STKPushResponse
-
-
 import com.example.darajastkpush.network.RetrofitClient
+
 
 class DarajaSTK(
     private val context: Context,
--- darajastkpush/src/main/java/com/example/darajastkpush/models/AccessTokenResponse.kt
+++ darajastkpush/src/main/java/com/example/darajastkpush/models/AccessTokenResponse.kt
@@ -1,7 +1,5 @@
 package com.example.darajastkpush.models
 
-import com.google.gson.annotations.SerializedName
-
 data class AccessTokenResponse(
     val access_token: String,
     val expires_in: String
